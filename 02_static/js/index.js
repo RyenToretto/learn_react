3d36c5873762407d162c22f77b653234f723b3ab
RyenToretto <18273727925@163.com>
2019-01-08 18:53:00 +0800

    react 从浅入深

---
+++ 02_static/js/index.js
@@ -0,0 +1,27 @@
+
+window.onload = function(){
+/*
+    // 鼠标跟随
+    var followDiv = document.getElementById("follow_mouse");
+    bindEventFunc(document, "mousemove", function(e){
+        followDiv.style.left = kjfMouse.getPageX(e) + 7 + "px";
+        followDiv.style.top = kjfMouse.getPageY(e) + 7 + "px";
+    });
+    
+    // 呼吸跟随
+    var followOpa = 1;
+    var followFlag = true;
+
+    var brath = setInterval(function() {
+        if (followOpa >= 0 && followFlag ) {
+            followOpa -= 0.1;
+        } else {
+            followOpa += 0.1;
+        }
+        followDiv.style.opacity = followOpa;
+        if(followOpa < 0 || followOpa > 1){
+            followFlag = !followFlag;
+        }
+    }, 200);
+*/
+};
